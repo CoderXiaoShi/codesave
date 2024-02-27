05022c6158f87d416ae08a450b6ecad78b24c208
xinglong.shi <xinglong.shi@sumscope.com>
2024-02-27 16:04:32 +0800

    update

--- func.js
+++ func.js
@@ -47,11 +47,11 @@ const autoPush = {
         autoPush.timer = setInterval(asyncGit, conf.pushInterval);
       }
     }
-    printMenu();
+    printMenu()
   }
 }
 
-options.set('1', autoPush)
+options.set('1', autoPush);
 
 options.set('2', {
   label: '2. 立刻同步代码',
